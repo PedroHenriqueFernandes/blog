35ea1af5d9e5254bd1115f7b63fa71dc103d4374
Pedro Henrique Fernandes <pedro007henrique41@gmail.com>
2023-01-26 14:59:18 -0300

    Fix: logo Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
-import Logo from '../../assets/logo.svg'
+import logoImage from '../../assets/logo.svg'
 import { HeaderContainer } from './styles'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <img src={Logo} alt="" />
+      <img src={logoImage} alt="" />
     </HeaderContainer>
   )
 }
